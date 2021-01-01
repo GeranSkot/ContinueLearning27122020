7f2cba465a277c77e147fb0bf67598e115024592
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-01-01 19:42:05 +0300

    35

---
+++ js/35.js
@@ -0,0 +1,9 @@
+
+
+const p = document.querySelectorAll('p');
+console.log(p);
+
+const scriptJS = document.createElement('scriptJS');
+scriptJS = "js/35.js";
+document.body.append(scriptJS);
+scriptJS.async = false;
